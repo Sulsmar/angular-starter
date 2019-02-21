3e98d261d05262519dfe11788a1603f9f5e06f80
Thomas Volk <tvolk@is-imac-7105.intern>
2019-02-21 17:04:06 +0100

    Erstellung erstzen von Inhalt durch Variablen

--- src/app/swa-register-page/swa-register-page.component.ts
+++ src/app/swa-register-page/swa-register-page.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwaRegisterPageComponent implements OnInit {
 
+  //Variablen Überschriften und Beschreibung
+    headingEvent: string = 'Veranstaltung "Mein Weg zum Energiedetektiv"';
+    headingAge: string = 'Für die 4.-7.Jahrgangsstufe';
+    description:string = 'Kurze Beschreibung, wenn geraucht. Bavaria ipsum dolor sit amet Freibia heitzdog Biawabn oamai.An anbandeln Wiesn, Resi etza Schneid dei.';
+
+  // Variablen Tag, Datum, Uhrzeit und Ort
+    dayOfEvent: string = 'Di';
+    dateOfEvent: string = '29.05.18';
+    timeOfEvent: string = 'von 13Uhr - 16Uhr';
+    placeOfEvent: string = 'Wunschort (optional)';
+    
   constructor() { }
 
   ngOnInit() {
